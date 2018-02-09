51b804cba75727aa2acc063ec3455f4d2c5d007f
Jarvis Fosdick <jarvis1@fastmail.fm>
2018-02-09 13:01:55 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+## These two functions provide simple example of lexical scope
 
 ## Write a short comment describing this function
-
+## creates getter / setter for cached value
 makeCacheMatrix <- function(x = matrix()) {
-          m <- NULL
+  m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
@@ -12,19 +12,17 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setsolve <- function(solve) m <<- solve
   getsolve <- function() m
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
-
+## looks for previous cached value else will try to 
+## apply the solve function to matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-          m <- x$getsolve()
-  print(m)
-  if(!is.null(m)) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if (!is.null(m)) {
     message("getting cached data")
     return(m)
   }
